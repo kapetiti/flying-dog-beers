138debd92812d00cd1e92cb601f5ed0c74d342ff
joaopedro-vm <joaopedrovm98@gmail.com>
2020-05-16 03:08:58 -0300

    test_form

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ app.layout = html.Div([
     [Input('my-slider', 'value')])
 def update_output(value):
 
-	return 'You have selected "{}"'.format(value), {"data": [dict(x=x1, y=np.sin(value*x1), name='sin({}x)'.format(value))]}
+	return 'You have selected "{}"'.format(value), {"data": [dict(x=x1, y=np.sin(value*x1), name='sin({})'.format(value))]}
 	
 
 if __name__ == '__main__':
