e25f1bec7e35860c776c706cf9eb23293263da30
joaopedro-vm <joaopedrovm98@gmail.com>
2020-05-16 02:18:56 -0300

    test_form

--- app.py
+++ app.py
@@ -7,47 +7,6 @@ app = dash.Dash(__name__)
 server = app.server
 
 app.layout = html.Div([
-   html.Label('Dropdown'),
-   dcc.Dropdown(
-      options=[
-         {'label': 'New York City', 'value': 'NYC'},
-         {'label': u'Montréal', 'value': 'MTL'},
-         {'label': 'San Francisco', 'value': 'SF'}
-      ],
-      value='MTL'
-   ),
-	
-   html.Label('Multi-Select Dropdown'),
-   dcc.Dropdown(
-      options=[
-         {'label': 'New York City', 'value': 'NYC'},
-         {'label': u'Montréal', 'value': 'MTL'},
-         {'label': 'San Francisco', 'value': 'SF'}
-      ],
-      value=['MTL', 'SF'],
-      multi=True
-   ),
-	
-   html.Label('Radio Items'),
-   dcc.RadioItems(
-      options=[
-         {'label': 'New York City', 'value': 'NYC'},
-         {'label': u'Montréal', 'value': 'MTL'},
-         {'label': 'San Francisco', 'value': 'SF'}
-      ],
-      value='MTL'
-   ),
-	
-   html.Label('Checkboxes'),
-   dcc.Checklist(
-      options=[
-         {'label': 'New York City', 'value': 'NYC'},
-         {'label': u'Montréal', 'value': 'MTL'},
-         {'label': 'San Francisco', 'value': 'SF'}
-      ],
-      values=['MTL', 'SF']
-   ),
-
    html.Label('Text Input'),
    dcc.Input(value='MTL', type='text'),
 	
@@ -58,7 +17,8 @@ app.layout = html.Div([
       marks={i: 'Label {}'.format(i) if i == 1 else str(i) for i in range(1, 6)},
       value=5,
    ),
-], style={'columnCount': 2})
+]
+)
 
 if __name__ == '__main__':
    app.run_server(debug=True)
