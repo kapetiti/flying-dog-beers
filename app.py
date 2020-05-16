a713aa962d625a83b6019808bf7dca6f8fb84182
joaopedro-vm <joaopedrovm98@gmail.com>
2020-05-16 13:26:04 -0300

    test

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import numpy as np
+import pandas as pd
 
+p = pd.read_csv(r"https://storage.googleapis.com/kagglesdsdata/datasets/549702/1159896/brazil_covid19.csv?GoogleAccessId=web-data@kaggle-161607.iam.gserviceaccount.com&Expires=1589853272&Signature=DKAi9dyP8LNUA7%2BOPiYrf8N23R4I5dr9sBW1iML1RQVFlyG3JfdI0S%2FRCQfgXYyYLW0Uni0ZsYkeQXLg1b5GfLIK8hehB%2B8cXJww1ZEBYCacwyPTonlcZ37GM0qOcZb3WmM5ctHt3vDEr4iIEN9p4gjuj8GqJvpsDvMt%2FrRtDP0dX%2FyKtQ8uCTARt%2BkStP7vjbV7CZBPuFeOBYdOt2Dk%2Bt%2F9qV6WgeMOnAFE1%2BkB5TKsNgNpqa%2BC8An5JR9%2BFwZS6%2B9i9Ng9h%2FLdswNY5UHgGlhtUoVegS%2F4VPWYkhcWQZU2DKGHPg9L5Raywxb4d5FDo%2FGpsK3z9iVLigoX8bsTjA%3D%3D&response-content-disposition=attachment%3B+filename%3Dbrazil_covid19.csv")
 x1 = np.linspace(0, 10, 1000)
 
 app = dash.Dash(__name__)
@@ -27,6 +29,12 @@ layout_home = html.Div([
 				style={"margin":{"l":0, "r":0, "t":0, "b":0}, "padding":10, "height":100, "width":300, "text-align":"center"},
 				children=[
 					dcc.Link([html.H1("sin(x)")], href="/page-sin"),
+				],
+			),
+			html.Div(
+				style={"margin":{"l":0, "r":0, "t":0, "b":0}, "padding":10, "height":100, "width":300, "text-align":"center"},
+				children=[
+					dcc.Link([html.H1("sin2(x)")], href="/page-sin2"),
 				],
 			),
 			html.Div(
@@ -67,6 +75,12 @@ layout_sin = html.Div([
 			html.Div(
 				style={"margin":{"l":0, "r":0, "t":0, "b":0}, "padding":10, "height":100, "width":300, "text-align":"center"},
 				children=[
+					dcc.Link([html.H1("sin2(x)")], href="/page-sin2"),
+				],
+			),
+			html.Div(
+				style={"margin":{"l":0, "r":0, "t":0, "b":0}, "padding":10, "height":100, "width":300, "text-align":"center"},
+				children=[
 					dcc.Link([html.H1("cos(x)")], href="/page-cos"),
 				],
 			),
@@ -95,6 +109,57 @@ layout_sin = html.Div([
 	),
 ])
 
+layout_sin2 = html.Div([
+	html.Div(
+		style={"height":100, "background-color":"#9015BD","display":"flex"},
+		children=[
+			html.Div(
+				style={"margin":{"l":0, "r":0, "t":0, "b":0}, "padding":10, "height":100, "width":300, "text-align":"center"},
+				children=[
+					dcc.Link([html.H1("SHIT IS REAL")], href="/"),
+				],
+			),
+			html.Div(
+				style={"margin":{"l":0, "r":0, "t":0, "b":0}, "padding":10, "height":100, "width":300, "text-align":"center"},
+				children=[
+					dcc.Link([html.H1("sin(x)")], href="/page-sin"),
+				],
+			),
+			html.Div(
+				style={"margin":{"l":0, "r":0, "t":0, "b":0}, "padding":10, "height":100, "width":300, "text-align":"center"},
+				children=[
+					dcc.Link([html.H1("sin2(x)")], href="/page-sin2"),
+				],
+			),
+			html.Div(
+				style={"margin":{"l":0, "r":0, "t":0, "b":0}, "padding":10, "height":100, "width":300, "text-align":"center"},
+				children=[
+					dcc.Link([html.H1("cos(x)")], href="/page-cos"),
+				],
+			),
+			html.Div(
+				style={"margin":{"l":0, "r":0, "t":0, "b":0}, "padding":10, "height":100, "width":300, "text-align":"center"},
+				children=[
+					html.A([html.H1("GitHub")], href="https://github.com/joaopedro-vm/flying-dog-beers", target="_blank"),
+				],
+			),
+		],
+	),
+	html.Div(style={"height":20}),
+	dcc.Graph(id='graph-sin2', figure=fig_sin),
+	html.Div(
+	[
+		html.P(id='drop-out-sin2', children=['Angular Frequency:']),    
+		dcc.Dropdown(
+			id="drop-freq-sin",
+			options=[{'label': i, 'value': i} for i in range(0, 21)],
+			value=1,
+		),
+	],
+	style={"width":500}
+	),
+])
+
 layout_cos = html.Div([
 	html.Div(
 		style={"height":100, "background-color":"#9015BD","display":"flex"},
@@ -109,6 +174,12 @@ layout_cos = html.Div([
 				style={"margin":{"l":0, "r":0, "t":0, "b":0}, "padding":10, "height":100, "width":300, "text-align":"center"},
 				children=[
 					dcc.Link([html.H1("sin(x)")], href="/page-sin"),
+				],
+			),
+			html.Div(
+				style={"margin":{"l":0, "r":0, "t":0, "b":0}, "padding":10, "height":100, "width":300, "text-align":"center"},
+				children=[
+					dcc.Link([html.H1("sin2(x)")], href="/page-sin2"),
 				],
 			),
 			html.Div(
@@ -161,6 +232,8 @@ app.validation_layout = html.Div([
 def display_page(pathname):
     if pathname == "/page-sin":
         return layout_sin
+    elif pathname == "/page-sin2":
+        return layout_sin2
     elif pathname == "/page-cos":
         return layout_cos
     else:
@@ -172,6 +245,14 @@ def display_page(pathname):
 def update_output(value):
 
 	return 'Angular Frequency: {}'.format(value), {"data": [{"x":x1, "y":np.sin(value*x1), "name":"sin({}x)".format(value), "showlegend":True}], "layout":{"width":500, "height":300, "margin":{"l":0, "r":0, "t":0, "b":0}}}
+
+@app.callback(
+    Output('graph-sin2', 'figure'),
+    [Input('drop-freq-sin', 'value')])
+def update_output(value):
+
+	return {"data": [{"x":x1, "y":np.sin(value*x1), "name":"sin({}x)".format(value), "showlegend":True}], "layout":{"width":500, "height":300, "margin":{"l":0, "r":0, "t":0, "b":0}}}
+
 
 @app.callback(
     [Output('slider-out-cos', 'children'), Output('graph-cos', 'figure')],
