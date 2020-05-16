e88576277b7f70b9f32ffeb7428f9b6e0b8761b1
joaopedro-vm <joaopedrovm98@gmail.com>
2020-05-16 02:15:54 -0300

    test_form

--- app.py
+++ app.py
@@ -1,31 +1,64 @@
+# -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
 
-df = pd.read_csv(
-   'https://gist.githubusercontent.com/chriddyp/'
-   'c78bf172206ce24f77d6363a2d754b59/raw/'
-   'c353e8ef842413cae56ae3920b8fd78468aa4cb2/'
-   'usa-agricultural-exports-2011.csv')
-	
-def generate_table(dataframe, max_rows=10):
-   return html.Table(
-      # Header
-      [html.Tr([html.Th(col) for col in dataframe.columns])] +
-      # Body
-      [html.Tr([
-         html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-      ]) for i in range(min(len(dataframe), max_rows))]
-   )
-	
 app = dash.Dash(__name__)
 server = app.server
-app.title = "njskdfnk"
-app.layout = html.Div(children=[
-   html.H4(children='US Agriculture Exports (2011)'),
-   generate_table(df)
-])
+
+app.layout = html.Div([
+   html.Label('Dropdown'),
+   dcc.Dropdown(
+      options=[
+         {'label': 'New York City', 'value': 'NYC'},
+         {'label': u'Montréal', 'value': 'MTL'},
+         {'label': 'San Francisco', 'value': 'SF'}
+      ],
+      value='MTL'
+   ),
+	
+   html.Label('Multi-Select Dropdown'),
+   dcc.Dropdown(
+      options=[
+         {'label': 'New York City', 'value': 'NYC'},
+         {'label': u'Montréal', 'value': 'MTL'},
+         {'label': 'San Francisco', 'value': 'SF'}
+      ],
+      value=['MTL', 'SF'],
+      multi=True
+   ),
+	
+   html.Label('Radio Items'),
+   dcc.RadioItems(
+      options=[
+         {'label': 'New York City', 'value': 'NYC'},
+         {'label': u'Montréal', 'value': 'MTL'},
+         {'label': 'San Francisco', 'value': 'SF'}
+      ],
+      value='MTL'
+   ),
+	
+   html.Label('Checkboxes'),
+   dcc.Checklist(
+      options=[
+         {'label': 'New York City', 'value': 'NYC'},
+         {'label': u'Montréal', 'value': 'MTL'},
+         {'label': 'San Francisco', 'value': 'SF'}
+      ],
+      values=['MTL', 'SF']
+   ),
+
+   html.Label('Text Input'),
+   dcc.Input(value='MTL', type='text'),
+	
+   html.Label('Slider'),
+   dcc.Slider(
+      min=0,
+      max=9,
+      marks={i: 'Label {}'.format(i) if i == 1 else str(i) for i in range(1, 6)},
+      value=5,
+   ),
+], style={'columnCount': 2})
 
 if __name__ == '__main__':
    app.run_server(debug=True)
